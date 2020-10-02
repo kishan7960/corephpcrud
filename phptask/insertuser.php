c84a2d8aef5b03fb464e0d0d7067cd30c8784972
kishan7960 <72219522+kishan7960@users.noreply.github.com>
2020-10-02 16:10:08 +0530

    Update insertuser.php

--- phptask/insertuser.php
+++ phptask/insertuser.php
@@ -18,16 +18,16 @@
 		<h2 class="text-center text-primary pt-5"> User SignUp Form</h2>
 		<form method="post" action="" class="pt-5">
 			<div class="form-group">
-			   <input type="text" name="TxtName" value="" placeholder="user name" class="form-control" required="">
+			   <input type="text" name="TxtName" value="" placeholder="user name" class="form-control" required>
 			</div>
 			<div class="form-group">
-			   <input type="email" name="TxtEmail" value="" placeholder="Email Address" class="form-control" required="">
+			   <input type="email" name="TxtEmail" value="" placeholder="Email Address" class="form-control" required>
 			</div>
 			<div class="form-group">
-			   <input type="password" name="TxtPassword" value="" placeholder="password" class="form-control" required="">
+			   <input type="password" name="TxtPassword" value="" placeholder="password" class="form-control" required>
 			</div>
 			<div class="form-group">
-			   <input type="text" name="TxtPhone" value="" placeholder="Phone No" class="form-control" required="">
+			   <input type="text" name="TxtPhone" value="" placeholder="Phone No" class="form-control" required>
 			</div>
 			<input type="submit" name="BtnSignup" value="Signup" class="btn btn-primary">
 		</form>
